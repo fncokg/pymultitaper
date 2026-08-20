40c57d8370aafd37cc9642cb2f4f6a4810e7886f
fncokg <kongchao1998@gmail.com>
2026-08-20 20:34:32 +0800

    add frame_masking feature

--- src/pymultitaper/utils.py
+++ src/pymultitaper/utils.py
@@ -4,7 +4,8 @@ from numpy.typing import NDArray
 from .backend import ArrayBackend
 
 
-def batched_signal(
+# TODO: we need the mask array of framed results, not of the raw signal.
+def batch_signals(
     signal_list: list,
     padding_strategy: Literal["max", "max_length"] = "max",
     max_length: Optional[int] = None,
@@ -26,7 +27,7 @@ def batched_signal(
     Returns:
         NDArray: A 2D array where each row corresponds to a signal from `signal_list`, padded with zeros as necessary.
     """
-    backend = ArrayBackend.like(signal_list[0])
+
     if padding_strategy == "max":
         max_len = max(signal.shape[0] for signal in signal_list)
     elif padding_strategy == "max_length":
@@ -45,13 +46,36 @@ def batched_signal(
     batched_array = backend.xp.zeros(
         (len(signal_list), max_len), dtype=signal_list[0].dtype
     )
-    mask = backend.xp.zeros((len(signal_list), max_len), dtype=bool)
+    mask = backend.xp.zeros((len(signal_list), max_len), dtype=backend.xp.int8)
 
     for i, signal in enumerate(signal_list):
         length = min(signal.shape[0], max_len)
         batched_array[i, :length] = signal[:length]
-        mask[i, :length] = True
+        mask[i, :length] = 1
 
     if return_mask:
         return batched_array, mask
     return batched_array
+
+
+def frame_masking(mask, fs, time_step, window_length=None, boundary_pad=False):
+    window_length = window_length if window_length is not None else time_step
+    n_tstep = int(time_step * fs)
+    n_winlen = int(window_length * fs)
+    backend = ArrayBackend.like(mask)
+    if boundary_pad:
+        n_pad = int(n_winlen / 2) + 1
+        pad_width = [(0, 0)] * mask.ndim
+        pad_width[-1] = (n_pad, n_pad)
+        mask = backend.xp.pad(
+            mask, pad_width=pad_width, mode="constant", constant_values=False
+        )
+    frames = backend.xp.lib.stride_tricks.sliding_window_view(
+        mask, n_winlen, writeable=False, axis=-1
+    )[..., ::n_tstep, :]
+    frame_mask = backend.xp.empty_like(frames, dtype=backend.xp.int8)
+    if boundary_pad:
+        backend.xp.any(frames == 1, axis=-1, out=frame_mask)
+    else:
+        backend.xp.all(frames == 1, axis=-1, out=frame_mask)
+    return frame_mask
