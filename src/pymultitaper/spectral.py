3ae336d0a1bef52e227abbb4c4dfb661898a84fc
fncokg <kongchao1998@gmail.com>
2026-08-20 18:05:41 +0800

    add feature: allow batched spectrogram computation

--- src/pymultitaper/spectral.py
+++ src/pymultitaper/spectral.py
@@ -120,7 +120,7 @@ def _spectrogram(
     The spectrogram is calculated when multiple window array and their weights are given. The result is the weighted sum of the PSDs of each windowed frame (when the weights sum to 1, it is the weighted average PSD). The computation backend (CPU or GPU) is automatically determined by the type of the input arrays (numpy or cupy).
 
     Args:
-        data (NDArray): (n_samples,) Input data. Can be either a numpy array or a cupy array. The backend (CPU or GPU) will be automatically determined based on the type of the input data.
+        data (NDArray): (..., n_samples) Input data. Can be either a numpy array or a cupy array. The backend (CPU or GPU) will be automatically determined based on the type of the input data.
         fs (float): Sampling frequency
         time_step (float): Time step between frames in seconds
         win (NDArray): (n_winlen,n_wins) Window arrays from different window functions. Should be of the same backend as `data`.
@@ -138,7 +138,7 @@ def _spectrogram(
     Returns:
         freqs (n_freqs,): Frequency points of the spectrogram
         times (n_frames,): Time points of each frame
-        psd (n_freqs,n_frames): PSD spectrogram
+        psd (..., n_freqs, n_frames): PSD spectrogram
     """
     # Prepare arguments
     # win: (n_winlen,n_wins)
@@ -155,30 +155,28 @@ def _spectrogram(
 
     if boundary_pad:
         n_pad = int(n_winlen / 2) + 1
-        data = backend.xp.pad(data, (n_pad, n_pad), "constant", constant_values=0)
-
+        # pad only the last dimension
+        data = backend.xp.pad(data, {-1: (n_pad, n_pad)}, "constant", constant_values=0)
     # Step 1: Frame the data
     # (n_frames,n_winlen)
     frames = backend.xp.lib.stride_tricks.sliding_window_view(
-        data, n_winlen, writeable=False
-    )[::n_tstep]
-    n_frames = frames.shape[0]
-
+        data, n_winlen, writeable=False, axis=-1
+    )[..., ::n_tstep, :]
+    n_frames = frames.shape[-2]
     # Step 2: Detrend (if necessary)
     if detrend != "off":
-        frames = backend.signal.detrend(frames, axis=1, type=detrend)
-
+        frames = backend.signal.detrend(frames, axis=-1, type=detrend)
     # Step 3: Windowing
-    # (n_frames,n_winlen,n_wins) = (n_frames,n_winlen,1) * (1,n_winlen,n_wins)
-    wined_frames = frames[..., None] * win[None, ...]
+    # broadcasting: (...,n_frames,n_winlen,n_wins) = (...,n_frames,n_winlen,1) * (n_winlen,n_wins)
+    wined_frames = frames[..., None] * win
 
     # Step 4: FFT
     nfft = (
         2 ** int(backend.xp.ceil(backend.xp.log2(n_winlen))) if nfft is None else nfft
     )
-    # (n_frames,nfft,n_wins)
+    # (...,n_frames,nfft,n_wins)
     # zero-padding is automatically done in `fft.rfft`
-    fft_data = backend.fft.rfft(wined_frames, n=nfft, axis=1)
+    fft_data = backend.fft.rfft(wined_frames, n=nfft, axis=-2)
 
     # Step 5: Calculate frequencies and time points
     raw_freqs = backend.fft.rfftfreq(nfft, 1 / fs)
@@ -188,8 +186,9 @@ def _spectrogram(
         times = backend.xp.arange(0, n_frames) * time_step
     else:
         times = backend.xp.arange(0, n_frames) * time_step + n_winlen / 2 / fs
+
     # Note: we filter out the frequencies with frequency range, therefore implicitly filter out the negative frequencies
-    fft_data = fft_data[:, freqs_idx, :]
+    fft_data = fft_data[..., freqs_idx, :]
 
     # Step 6: Calculate PSD and average over window types
     # (n_wins,) We need to scale the PSD by the sum of the square of the window and fs
@@ -205,8 +204,8 @@ def _spectrogram(
         psd_data[:, -1] /= 2
     if db_scale:
         psd_data = 10 * backend.xp.log10(psd_data / p_ref**2)
-    # (nfft,n_frames)
-    psd_data = psd_data.T
+    # (...,nfft,n_frames)
+    psd_data = np.swapaxes(psd_data, -1, -2)
     return freqs, times, psd_data
 
 
@@ -231,7 +230,9 @@ def multitaper_spectrogram(
     The computation backend (CPU or GPU) is automatically determined by the type of the input arrays (numpy or cupy).
 
     Args:
-        data (NDArray): (n_samples,) Input data. Can be either a numpy array or a cupy array. The backend (CPU or GPU) will be automatically determined based on the type of the input data.
+        data (NDArray): (..., n_samples) Input data:
+            - Can be of any shape **(including 1d array)**, as long as the last dimension is the time dimension (n_samples). The spectrogram computation will be applied to the last dimension, and the other dimensions will be treated as batch dimensions and preserved in the output.
+            - Can be either a numpy array or a cupy array. The backend (CPU or GPU) will be automatically determined based on the type of the input data.
         fs (float): Sampling frequency
         time_step (float): Time step between frames in seconds
         window_length (float, optional): Window length in seconds. If `None`, will be set to the same as `time_step`. Defaults to None.
@@ -252,9 +253,9 @@ def multitaper_spectrogram(
         The value of 2W is the regularization bandwidth. Typically, we choose W to be a small multiple of the fundamental frequency 1/(N*dt) (where N is the number of samples in the data), i.e. W=i/(N*dt). The value of the parameter `NW` here is in fact the value of i (when dt is seen as 1). There's a trade-off between frequency resolution and variance reduction: A larger `NW` will reduce the variance of the PSD estimate, but also reduce the frequency resolution.
 
     Returns:
-        freqs (NDArray): (n_freqs,) Frequency points of the spectrogram
-        times (NDArray): (n_frames,) Time points of each frame
-        psd (NDArray): (n_freqs,n_frames) PSD spectrogram
+        freqs (NDArray): (n_freqs,) Frequency points of the spectrogram.
+        times (NDArray): (n_frames,) Time points of each frame.
+        psd (NDArray): (..., n_freqs, n_frames) PSD spectrogram. The shape of the output PSD is the same as the input data, except that the last dimension (time) is replaced by the frequency and time dimensions.
 
     Examples:
         >>> freqs,times,psd = multitaper_spectrogram(data,fs,time_step=0.001,window_length=0.005,NW=4)
@@ -301,7 +302,9 @@ def spectrogram(
     This is similar to `scipy.signal.spectrogram` except that it supports also GPU computation. The computation backend (CPU or GPU) is automatically determined by the type of the input arrays (numpy or cupy).
 
     Args:
-        data (NDArray): (n_samples,) Input data. Can be either a numpy array or a cupy array. The backend (CPU or GPU) will be automatically determined based on the type of the input data.
+        data (NDArray): (..., n_samples) Input data:
+            - Can be of any shape **(including 1d array)**, as long as the last dimension is the time dimension (n_samples). The spectrogram computation will be applied to the last dimension, and the other dimensions will be treated as batch dimensions and preserved in the output.
+            - Can be either a numpy array or a cupy array. The backend (CPU or GPU) will be automatically determined based on the type of the input data.
         fs (float): Sampling frequency
         time_step (float): Time step between frames in seconds
         window_length (float, optional): Window length in seconds. If `None`, will be set to the same as `time_step`. Defaults to None.
@@ -317,9 +320,9 @@ def spectrogram(
             - If `False`, the first frame is centered at `window_length/2` seconds after the first sample, and samples after `n_frames*time_step+window_length` seconds are ignored.
 
     Returns:
-        freqs (NDArray): (n_freqs,) Frequency points of the spectrogram
-        times (NDArray): (n_frames,) Time points of each frame
-        psd (NDArray): (n_freqs,n_frames) PSD spectrogram
+        freqs (NDArray): (n_freqs,) Frequency points of the spectrogram.
+        times (NDArray): (n_frames,) Time points of each frame.
+        psd (NDArray): (..., n_freqs, n_frames) PSD spectrogram. The shape of the output PSD is the same as the input data, except that the last dimension (time) is replaced by the frequency and time dimensions.
 
     Examples:
         >>> freqs,times,psd = spectrogram(data,fs,time_step=0.001,window_length=0.005)
