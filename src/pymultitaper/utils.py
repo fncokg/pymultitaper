9dbe1d3660b8dad1726d851ea222cea8e4610732
fncokg <kongchao1998@gmail.com>
2026-08-20 18:33:02 +0800

    support return mask in batched_signal

--- src/pymultitaper/utils.py
+++ src/pymultitaper/utils.py
@@ -5,7 +5,7 @@ from .backend import ArrayBackend
 
 
 def batched_signal(
-    data_list: list,
+    signal_list: list,
     padding_strategy: Literal["max", "max_length"] = "max",
     max_length: Optional[int] = None,
     return_mask: bool = False,
@@ -14,17 +14,18 @@ def batched_signal(
     Convert a list of 1D signals into a 2D array with padding.
 
     Args:
-        data_list (list): A list of 1D numpy or cupy arrays.
+        signal_list (list): A list of 1D numpy or cupy arrays.
         padding_strategy (str): The strategy for padding. Options are:
             - "max": Pad all signals to the length of the longest signal in the list.
             - "max_length": Pad all signals to the specified `max_length`. If a signal is longer than `max_length`, it will be truncated.
         max_length (int, optional): The maximum length to pad/truncate signals to when using the "max_length" strategy. Required if `padding_strategy` is "max_length".
 
     Returns:
-        NDArray: A 2D array where each row corresponds to a signal from `data_list`, padded with zeros as necessary.
+        NDArray: A 2D array where each row corresponds to a signal from `signal_list`, padded with zeros as necessary.
     """
+    backend = ArrayBackend.like(signal_list[0])
     if padding_strategy == "max":
-        max_len = max(signal.shape[0] for signal in data_list)
+        max_len = max(signal.shape[0] for signal in signal_list)
     elif padding_strategy == "max_length":
         if max_length is None:
             raise ValueError(
@@ -35,15 +36,19 @@ def batched_signal(
         raise ValueError(f"Unsupported padding strategy: {padding_strategy}")
 
     # Determine the backend based on the first signal
-    backend = ArrayBackend.like(data_list[0])
+    backend = ArrayBackend.like(signal_list[0])
 
     # Create an empty array with the appropriate shape and backend
     batched_array = backend.xp.zeros(
-        (len(data_list), max_len), dtype=data_list[0].dtype
+        (len(signal_list), max_len), dtype=signal_list[0].dtype
     )
+    mask = backend.xp.zeros((len(signal_list), max_len), dtype=bool)
 
-    for i, signal in enumerate(data_list):
+    for i, signal in enumerate(signal_list):
         length = min(signal.shape[0], max_len)
         batched_array[i, :length] = signal[:length]
+        mask[i, :length] = True
 
+    if return_mask:
+        return batched_array, mask
     return batched_array
